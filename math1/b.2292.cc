cfb0e5fcc1843109af6c447b3ce9248779493b30
miney727 <miney0727@naver.com>
2020-01-15 23:31:08 +0900

    Create b.2292.cc

---
+++ math1/b.2292.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int a;
+	int b = 0, c =0;
+	cin >> a;
+	if (a == 1)
+	{
+		cout << 1 << endl;
+		return 0;
+	}
+	for (a = a; a > 6*c+1; b++)
+	{
+		c =c+ b;
+	}
+	cout << b << endl; 
+	return 0;
+
+}
