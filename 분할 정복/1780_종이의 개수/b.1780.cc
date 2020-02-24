70c7c903c446299ad27e27f0e4e108934b7f6e77
miney727 <miney0727@naver.com>
2020-02-25 06:05:45 +0900

    Create b.1780.cc

---
+++ 분할 정복/1780_종이의 개수/b.1780.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[2188][2188];
+int one = 0, zero = 0, minus = 0;
+
+void paper(int x, int y, int leng)
+{
+	int temp = arr[x][y];
+	bool pure = true;
+	for (int i = x; i < x + leng; i++)
+	{
+		for (int j = y; j < y + leng; j++)
+		{
+			if (temp != arr[i][j] && pure)//순수하지 않을시 9등분
+			{
+				paper(x, y, leng / 3);
+				paper(x + leng / 3, y, leng / 3);
+				paper(x + leng*2 /3, y, leng / 3);
+				paper(x, y + leng / 3, leng / 3);
+				paper(x, y + leng*2 / 3, leng / 3);
+				paper(x + leng / 3, y + leng / 3, leng / 3);
+				paper(x + leng*2 / 3, y + leng / 3, leng / 3);
+				paper(x + leng / 3, y + leng*2 / 3, leng / 3);
+				paper(x + leng*2 / 3, y + leng*2 / 3, leng / 3);
+				pure = false;
+			}
+		}
+	}
+	if (pure)
+	{
+		if (temp == 1)
+		{
+			one++;
+		}
+		else if (temp == 0)
+		{
+			zero++;
+		}
+		else
+		{
+			::minus++;
+		}
+	}
+}
+
+int main()
+{
+	int N, c;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+			cin >> c;
+			arr[i][j] = c;
+		}
+	}
+	paper(1, 1, N);
+	cout << ::minus << endl << zero << endl << one << endl;
+	return 0;
+}
